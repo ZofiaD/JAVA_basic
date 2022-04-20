b3d19f8740118a50f2382e97a4c7890956df09ca
Zofia D <zszszs@example.com>
2022-04-20 21:04:31 +0200

    commitSwitch

---
+++ src/Switch.java
@@ -0,0 +1,29 @@
+public class Switch {
+    public static void main(String[] args) {
+        int a = 11;
+        switch(a){
+            case 1:
+            System.out.println("poniedziałek");
+                break;
+            case 2:
+            System.out.println("wtorek");
+                break;
+            case 3:
+                System.out.println("środa");
+                break;
+            case 4:
+                System.out.println("czwartek");
+                break;
+            case 5:
+                System.out.println("piątek");
+                break;
+            case 6:
+                System.out.println("sobota");
+                break;
+            default:
+                System.out.println("papa");
+
+
+        }
+    }
+}
