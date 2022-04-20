8bf45b791ed2e7bcb155ab51c0819fa8abb7baae
Zofia D <zszszs@example.com>
2022-04-20 20:18:44 +0200

    Java

---
+++ src/ForEach.java
@@ -0,0 +1,13 @@
+public class ForEach {
+    public static void main(String[] args) {
+        int[] tablica = new int[10];
+        for( int i = 0; i< tablica.length; i+=2){
+            tablica[i]=i+1;
+
+        }
+        for (int zmienna:tablica){
+            System.out.println(zmienna);
+
+        }
+    }
+}
---
+++ src/PetlaDoWhile.java
@@ -0,0 +1,11 @@
+public class PetlaDoWhile {
+    public static void main(String[] args) {
+        int a = 0;
+        do {
+            System.out.println("Zosia jest super");
+            a++;
+        }
+            while(a < 10);
+            System.out.println("Koniec");
+    }
+}
