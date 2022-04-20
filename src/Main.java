afa1f82a193f78bd38c1872fce334250f402af5a
Zofia D <zszszs@example.com>
2022-04-20 17:40:33 +0200

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String month = "april";
-        int year = 2022;
-        long day = 19;
-        System.out.println("miesiąc:" + month);
-        System.out.println("rok:" + year);
-        System.out.println("dzień:" + day);
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Jak masz na imię?");
+      String name = scanner.nextLine();
+      System.out.println("Cześć, " + name + " Jak masz na nazwisko?");
+      String surname = scanner.nextLine();
+      System.out.println(surname + " fajne nazwisko, a ile masz lat?");
+      int age = scanner.nextInt();
+      if (age >= 18) {
+          System.out.println(age + " ale super! To kupisz mi piwo?");
+      } else {
+          System.out.println(age + " to lipa, dzisiaj nie pijemy");
+      }
 
     }
 }
